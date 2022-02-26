042c242b122a6b98aca3b4f78b880f3b5cee964c
NakimuliSusan <nakimulimarysusan@gmail.com>
2022-02-26 23:20:41 +0300

    my work

---
+++ src/main.kt
@@ -0,0 +1,22 @@
+fun main () {
+    var text = "akirachix"
+    var result =  (text[0].toString()+ text[2] + text[3])
+    println(result)
+
+    var x = "Verite"
+    var y = 24
+    println("Hi, my name is $x and I am $y old ")
+
+    var a = "bonne chance"
+    val stringLength = a.length
+    println(stringLength)
+
+    var name = "Verite"
+    var name1 = "Verite"
+    if (name==name1)
+    println("That's me!")
+    else
+        println("I don't know who that is")
+
+
+}
